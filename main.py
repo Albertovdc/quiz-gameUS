34d551b3043a4a8ee9d1e4d2e6ce22be385dca43
Albertovdc <albertovdc.gnz@gmail.com>
2024-01-29 12:50:52 -0600

    Update

--- main.py
+++ main.py
@@ -7,14 +7,33 @@ screen.title("Quiz Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
-user_answer = screen.textinput("Guess the state", "Enter the satate")
+game_on = True
+while game_on:
+  user_answer = screen.textinput("Guess the state", "Enter the satate")
 
-# Extract the states from de csv
-data = pandas.read_csv("50_states.csv")
+  # Extract the states from de csv
+  data = pandas.read_csv("50_states.csv")
 
-# Get the states in a list
-states = data.state.to_list()
-print(states)
+  # Get the states in a list
+  states = data.state.to_list()
+  # print(states)
 
+  # Check if the user answer is in the list of the states
+  if user_answer in states:
+    # print("There is")
+    # Print in the coords
+    # print(data[data.state == user_answer])
+    coord_data = data[data.state == user_answer]
+    # print(coord_data)
+    # Get the x and y values
+    x_coord = coord_data.x.item()
+    # print(x_coord)
+    y_coord = coord_data.y.item()
+
+    item_state = turtle.Turtle()
+    item_state.hideturtle()
+    item_state.penup()
+    item_state.goto(x_coord, y_coord)
+    item_state.write(f"{user_answer}", align="center", font=("Courier", 9, "normal"))
 
 screen.exitonclick()
