46759e431e0c68a930da077b7671ffc0dbb482ba
Albertovdc <albertovdc.gnz@gmail.com>
2024-01-30 13:34:42 -0600

    Update final

--- main.py
+++ main.py
@@ -7,19 +7,26 @@ screen.title("Quiz Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+
+user_correct_answer = 0
 game_on = True
+user_answer = screen.textinput("Guess the state", "Enter the satate")
+user_answer.title()
+# Extract the states from de csv
+data = pandas.read_csv("50_states.csv")
+
+# Get the states in a list
+states = data.state.to_list()
+# print(states)
+num_states = len(states)
 while game_on:
-  user_answer = screen.textinput("Guess the state", "Enter the satate")
-
-  # Extract the states from de csv
-  data = pandas.read_csv("50_states.csv")
-
-  # Get the states in a list
-  states = data.state.to_list()
-  # print(states)
 
   # Check if the user answer is in the list of the states
   if user_answer in states:
+    user_correct_answer += 1
+    if user_correct_answer == 50:
+      game_on = False
+    states.remove(user_answer)
     # print("There is")
     # Print in the coords
     # print(data[data.state == user_answer])
@@ -35,5 +42,7 @@ while game_on:
     item_state.penup()
     item_state.goto(x_coord, y_coord)
     item_state.write(f"{user_answer}", align="center", font=("Courier", 9, "normal"))
+
+  user_answer = screen.textinput(f"{user_correct_answer} / {num_states}", "Enter the satate").title()
 
 screen.exitonclick()
